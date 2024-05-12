e1dd3296d67d13a8b22a2b48e34452fbba6aa50d
Reyko2 <rico.nieto.cics@ust.edu.ph>
2024-05-12 23:48:21 +0800

    discover page frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,45 +5,57 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 
 function Navbar() {
-    return (
-        <nav className={`fixed-top ${styles.navbar}`}>
+  return (
+    <nav className={`fixed-top ${styles.navbar}`}>
+      <div className={`${styles.main}`}>
+        {/* LINKS */}
+        <div className={`col-1 ${styles.navcol}`}>
+          <Link to="/homepage" className={` ${styles.link}`}>
+            home
+          </Link>
+        </div>
 
-            <div className={`${styles.main}`}>
-                
-                {/* LINKS */}
-                <div className={`col-1 ${styles.navcol}`}>
-                    <Link to = "/homepage" className={` ${styles.link}`}>home</Link>
-                </div>
+        <div className={`col-1 ${styles.navcol}`}>
+          <Link to="/discover" className={` ${styles.link}`}>
+            discover
+          </Link>
+        </div>
 
-                <div className={`col-1 ${styles.navcol}`}>
-                    <a href="" className={` ${styles.link}`}>discover</a>
-                </div>
+        <div className={`col-1 ${styles.navcol}`}>
+          <Link to="/trending" className={` ${styles.link}`}>
+            {" "}
+            trending{" "}
+          </Link>
+        </div>
 
-                <div className={`col-1 ${styles.navcol}`}>
-                    <Link to = "/trending"  className={` ${styles.link}`}> trending </Link>
-                </div>
+        {/* LOGO */}
+        <div className="col-4">
+          <a href="">
+            <img src={logo} alt="logo" className={styles.logo} />
+          </a>
+        </div>
 
-                    {/* LOGO */}
-                    <div className="col-4">
-                        <a href=""><img src={logo} alt="logo" className={styles.logo} /></a>
-                    </div>
+        {/* LINKS */}
+        <div className={`col-1 ${styles.navcol}`}>
+          <a href="" className={` ${styles.link}`}>
+            notifications
+          </a>
+        </div>
 
-                {/* LINKS */}
-                <div className={`col-1 ${styles.navcol}`}>
-                    <a href="" className={` ${styles.link}`}>notifications</a>
-                </div>
+        <div className={`col-1 ${styles.navcol}`}>
+          <Link to="/profile" className={` ${styles.link}`}>
+            profile
+          </Link>
+        </div>
 
-                <div className={`col-1 ${styles.navcol}`}>
-                    <Link to = "/profile" className={` ${styles.link}`}>profile</Link>
-                </div>
-
-                <div className={`col-1 ${styles.navcol}`}>
-                    <a href="" className={` ${styles.link}`}>logout</a>
-                </div>
-            </div>
-            
-        </nav>
-    )
+        <div className={`col-1 ${styles.navcol}`}>
+          <a href="" className={` ${styles.link}`}>
+            logout
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
